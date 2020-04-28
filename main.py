da9c880eb6c929de9c96151284e97ec3666d1d80
davidondari <davidondari12@gmail.com>
2020-04-28 22:34:56 +0300

    removed some code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-import requests
 
-print(sys.version)
-print(sys.executable)
+hallo = open("C:\\USERS\\HP 15\\DESKTOP\\PYTHON PROJECT\\sayhi.txt", "w")
+hallo.write('we are not the same,Im a martian \n')
+hallo.close()
+
+hallo = open('sayhi.txt')
+print(hallo.readlines())
+hallo.close()
